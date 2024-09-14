5ce3f31b1ccb5d64ea5e395a5a37fe32429257a6
JoaoG780 <joaobielsilva780@gmail.com>
2024-09-14 15:25:56 -0300

    Create QUESTÃO 02.c

---
+++ QUESTÃO 02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int numeros[] = {5, 1, 4, 2, 8, 3, 6, 7};
+    int tamanho = 8;
+    int i, j, temp;
+    int troca = 0;
+
+    for (i = 0; i < tamanho - 1; i++) {
+
+        for (j = 0; j < tamanho - i - 1; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+
+                temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+                troca++;
+            }
+        }
+
+        if (troca == 0) {
+            break;
+        }
+    }
+
+    printf("Array em ordem crescente: ");
+    for (i = 0; i < tamanho; i++) {
+        printf("%d ", numeros[i]);
+    }
+
+    return 0;
+}
